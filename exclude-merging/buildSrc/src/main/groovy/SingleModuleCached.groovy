0a9d722db2d05b627024f084689e2e6038b3245a
Cedric Champeau <cedric.champeau@gmail.com>
2018-05-17 11:22:56 +0200

    Add different implementations of component metadata suppliers
    This commit adds alternate implementations of component metadata suppliers,
    for test purposes. The following implementations are available:
    
    - `AllVersionsAtOnce`: a single resource, `versions.txt` is used to list all versions
    of all modules
    - `AllVersionsAtOnceCached`: the same as above, but with caching enabled (Gradle 4.9+)
    - `SingleModule`: each module (group, artifact) has its own `versions.txt` file
    - `SingleModuleCached`: same as above, but with caching enabled (Gradle 4.9+)
    
    For backwards compatibility, we have copied the `@CacheableRule` annotation.

---
+++ exclude-merging/buildSrc/src/main/groovy/SingleModuleCached.groovy
@@ -0,0 +1,36 @@
+import groovy.transform.CompileStatic
+import org.gradle.api.artifacts.ComponentMetadataSupplier
+import org.gradle.api.artifacts.ComponentMetadataSupplierDetails
+import org.gradle.api.artifacts.repositories.RepositoryResourceAccessor
+import org.gradle.api.artifacts.CacheableRule
+
+@CompileStatic
+@CacheableRule
+class SingleModuleCached implements ComponentMetadataSupplier {
+        private final RepositoryResourceAccessor repositoryResourceAccessor
+
+        @javax.inject.Inject
+        public SingleModuleCached(RepositoryResourceAccessor accessor) {
+            this.repositoryResourceAccessor = accessor
+        }
+        
+        void execute(ComponentMetadataSupplierDetails details) {
+            def id = details.id
+            def status = [:]
+            repositoryResourceAccessor.withResource("${id.group}/${id.module}/versions.txt") {
+                it.withReader { reader ->
+                    reader.eachLine { line ->
+                        if (line) {
+                           def split = line.split(';')
+                           def module = split[0]
+                           def st = split[1]
+                           status[module] = st
+                        }
+                    }
+                }
+            }
+            details.result.status = status[id.toString()]
+            details.result.statusScheme = ['integration', 'milestone', 'candidate', 'release']
+        }
+}
+
